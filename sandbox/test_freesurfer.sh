146770ff163bb63852cd2f884c095633a14ff2ac
Kent-Andre Mardal <kent-and@login-0-0.local>
2016-04-17 12:09:59 +0200

    freesurfer job script seems to work

--- sandbox/test_freesurfer.sh
+++ sandbox/test_freesurfer.sh
@@ -5,7 +5,7 @@
 # Project:
 #SBATCH --account=nn9279k
 # Wall clock limit:
-#SBATCH --time='01:00:00'
+#SBATCH --time='06:00:00'
 #
 # Max memory usage per task:
 #SBATCH --mem-per-cpu=3800M
@@ -15,14 +15,23 @@
 #SBATCH --cpus-per-task=1
 
 #SBATCH --partition=long
-#SBATCH --output=output.$SCRATCH 
-
+##SBATCH --output=output.$SCRATCH 
+source /cluster/bin/jobsetup
+module load freesurfer
+#chkfile subjects 
 
 export FREESURFER_HOME=$HOME/freesurfer 
-export PATH=$FREESURFER_HOME/bin:$PATH
-export SUBJECTS_DIR=$FREESURFER_HOME/subjects
+export SUBJECTS_DIR=$SCRATCH
+#export PATH=$FREESURFER_HOME/bin:$PATH
+echo $SCRATCH
+echo `date`
 
-recon-all -subjid erika -i  $SUBJECTS_DIR/MRI_ERIKA  
+cp -r $FREESURFER_HOME/subjects/MRI_ERIKA $SCRATCH
+ls $SCRATCH 
+ls $SCRATCH/MRI_ERIKA/*
+
+recon-all -subjid erika -i   $SCRATCH/MRI_ERIKA/Dicom/IM_0594  -all 
 
 
+echo `date`
 
